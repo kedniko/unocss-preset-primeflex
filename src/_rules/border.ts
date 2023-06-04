e335be25ac3fa274d4843debee215dee05ada190
kedniko <42846752+kedniko@users.noreply.github.com>
2023-06-04 13:17:06 +0200

    feat: border

--- src/_rules/border.ts
+++ src/_rules/border.ts
@@ -1,2 +1,16 @@
+
+const borderRadius = []
+
+const borderWidth = []
+
+const borderStyle = [
+  [/^border-(solid|dashed|dotted|double)$/, ([, c]) => ({ 'border-style': c }), { autocomplete: 'border-(solid|dashed|dotted|double)' }],
+]
+const borderColor = []
+
 export default [
+  ...borderRadius,
+  ...borderWidth,
+  ...borderStyle,
+  ...borderColor,
 ] as any
