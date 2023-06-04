aa600da4c0c32a12fbfaba4ca590cc762b643dc5
kedniko <42846752+kedniko@users.noreply.github.com>
2023-06-04 13:17:53 +0200

    feat: utils

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -5,3 +5,15 @@ export const range = (start: number, end: number, fn: (i: number) => any, option
   }
   return result as any[];
 }
+
+export const mapUnitToRem = {
+  '0': '0',
+  '1': '0.25rem',
+  '2': '0.5rem',
+  '3': '1rem',
+  '4': '1.5rem',
+  '5': '2rem',
+  '6': '3rem',
+  '7': '4rem',
+  '8': '5rem',
+}
