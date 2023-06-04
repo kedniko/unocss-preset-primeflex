95f5f602d0dec800a26e6d19263f0f5edffa0950
kedniko <42846752+kedniko@users.noreply.github.com>
2023-06-04 13:17:31 +0200

    feat: spacing

--- src/_rules/spacing.ts
+++ src/_rules/spacing.ts
@@ -1,2 +1,31 @@
+import { mapUnitToRem, range } from "../_utils"
+
+const padding = (() => {
+  const values = range(0, 8, n => [
+    [`p-${n}`, { 'padding': mapUnitToRem[n] }],
+    [`pt-${n}`, { 'padding-top': mapUnitToRem[n] }],
+    [`pr-${n}`, { 'padding-right': mapUnitToRem[n] }],
+    [`pb-${n}`, { 'padding-bottom': mapUnitToRem[n] }],
+    [`pl-${n}`, { 'padding-left': mapUnitToRem[n] }],
+    [`px-${n}`, { 'padding-left': mapUnitToRem[n], 'padding-right': mapUnitToRem[n] }],
+    [`py-${n}`, { 'padding-top': mapUnitToRem[n], 'padding-bottom': mapUnitToRem[n] }],
+  ])
+  return values.flat()
+})()
+const margin = (() => {
+  const values = range(0, 8, n => [
+    [`m-${n}`, { 'margin': mapUnitToRem[n] }],
+    [`mt-${n}`, { 'margin-top': mapUnitToRem[n] }],
+    [`mr-${n}`, { 'margin-right': mapUnitToRem[n] }],
+    [`mb-${n}`, { 'margin-bottom': mapUnitToRem[n] }],
+    [`ml-${n}`, { 'margin-left': mapUnitToRem[n] }],
+    [`mx-${n}`, { 'margin-left': mapUnitToRem[n], 'margin-right': mapUnitToRem[n] }],
+    [`my-${n}`, { 'margin-top': mapUnitToRem[n], 'margin-bottom': mapUnitToRem[n] }],
+  ])
+  return values.flat()
+})()
+
 export default [
+  ...padding,
+  ...margin,
 ] as any
